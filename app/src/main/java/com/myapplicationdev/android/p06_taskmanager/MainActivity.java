3b5575ec00729be1320d9fce78d9d9d954fab844
18004885 <18004885@myrp.edu.sg>
2020-08-14 16:56:44 +0800

    Final

--- app/src/main/java/com/myapplicationdev/android/p06_taskmanager/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p06_taskmanager/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == actReqCode) {
